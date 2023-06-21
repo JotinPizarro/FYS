a6200ddddbce4b544a5d521dc5f1a1868fa06816
JotinPizarro <137328822+JotinPizarro@users.noreply.github.com>
2023-06-21 15:50:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = () => navbar.classList.toggle("active");
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = () => navbar.classList.remove("active");
+
+addEventOnElem(navLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn active when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+
+const inputQuantity = document.querySelector('.input-quantity');
+const btnIncrement = document.querySelector('#increment');
+const btnDecrement = document.querySelector('#decrement');
+
+let valueByDefault = parseInt(inputQuantity.value);
+
+// Funciones Click
+
+btnIncrement.addEventListener('click', () => {
+	valueByDefault += 1;
+	inputQuantity.value = valueByDefault;
+});
+
+btnDecrement.addEventListener('click', () => {
+	if (valueByDefault === 1) {
+		return;
+	}
+	valueByDefault -= 1;
+	inputQuantity.value = valueByDefault;
+});
+
+// Toggle
+// Constantes Toggle Titles
+const toggleDescription = document.querySelector(
+	'.title-description'
+);
+const toggleAdditionalInformation = document.querySelector(
+	'.title-additional-information'
+);
+const toggleReviews = document.querySelector('.title-reviews');
+
+// Constantes Contenido Texto
+const contentDescription = document.querySelector(
+	'.text-description'
+);
+const contentAdditionalInformation = document.querySelector(
+	'.text-additional-information'
+);
+const contentReviews = document.querySelector('.text-reviews');
+
+// Funciones Toggle
+toggleDescription.addEventListener('click', () => {
+	contentDescription.classList.toggle('hidden');
+});
+
+toggleAdditionalInformation.addEventListener('click', () => {
+	contentAdditionalInformation.classList.toggle('hidden');
+});
+
+toggleReviews.addEventListener('click', () => {
+	contentReviews.classList.toggle('hidden');
+});
+
+
+
+/**
+ * filter function
+ */
+
+const filterBtns = document.querySelectorAll("[data-filter-btn]");
+const filterItems = document.querySelectorAll("[data-filter]");
+
+let lastClickedFilterBtn = filterBtns[0];
+
+const filter = function () {
+  lastClickedFilterBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedFilterBtn = this;
+
+  for (let i = 0; i < filterItems.length; i++) {
+    if (this.dataset.filterBtn === filterItems[i].dataset.filter ||
+      this.dataset.filterBtn === "all") {
+
+      filterItems[i].style.display = "block";
+      filterItems[i].classList.add("active");
+
+    } else {
+
+      filterItems[i].style.display = "none";
+      filterItems[i].classList.remove("active");
+
+    }
+  }
+}
+
+addEventOnElem(filterBtns, "click", filter);
